a0c24285acbaa01929d984d4b0140f4eebec695b
422404 <elyanpoujol@gmail.com>
2019-07-15 23:51:46 +0200

    Token generator also generates an array with their names.

--- Grammar/tokens_gen.py
+++ Grammar/tokens_gen.py
@@ -24,11 +24,18 @@ def gen_tokens(tokens_file, token_h_file):
             for (token_name, token_value) in tokens:
                 if token_value:
                     hf.write("#define TOK_" + token_name + (" " + token_value if token_value else "") + "\n")
-            hf.write("\ntypedef enum {\n"
-                     "    TOKTYPE_NOTTOKEN = -1,\n")
+            
+            hf.write("\ntypedef enum {\n")
             for (token_name, _) in tokens:
                 hf.write("    TOKTYPE_" + token_name + ",\n")
-            hf.write("    TOKENS_NUMBER\n} token_type_t;\n")
+            
+            hf.write("    TOKENS_NUMBER,\n"
+                     "    TOKTYPE_NOTTOKEN\n"
+                     "} token_type_t;\n\n"
+                     "char * token_type_names[] = {\n")
+            for (token_name, _) in tokens:
+                hf.write("    \"TOKTYPE_" + token_name + "\",\n")
+            hf.write("};\n")
 
 if __name__ == "__main__":
     gen_tokens(sys.argv[1], sys.argv[2])
